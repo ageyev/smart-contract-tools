2a806f46a93da511e95fef89c99922072047e549
Viktor <ageyev@gmail.com>
2018-04-05 12:17:46 +0300

    add truffle contract

---
+++ merge.dev.and.master.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# see:
+# https://stackoverflow.com/questions/14168677/merge-development-branch-with-master
+
+# to create new dev branch:
+# git checkout -b dev
+
+git branch
+git add . && git commit -a
+
+git checkout dev
+# on branch development:
+git merge master
+# (resolve any merge conflicts if there are any)
+git checkout master
+git merge --no-ff dev
+# (there won't be any conflicts now)
+git checkout dev
+# (back to dev)
+
